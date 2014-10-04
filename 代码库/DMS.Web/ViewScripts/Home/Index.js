f7d97269c2879b2cf24a80c6bda4dea67e41567e
jason <190187776@qq.com>
2014-10-04 14:47:14 +0800

    1

---
+++ 代码库/DMS.Web/ViewScripts/Home/Index.js
@@ -0,0 +1,27 @@
+﻿$(function () {
+    jQuery("#RightAccordion").accordion({ //初始化accordion
+        fillSpace: true,
+        fit: true,
+        border: false,
+        animate: false
+    });
+    $('#RightAccordion').accordion('add', {
+        title: '系统管理',
+        content: "<p>Accordion is a part of easyui framework for jQuery. It lets you define your accordion component on web page more easily.</p>",
+        selected: true,
+        iconCls: 'icon-ok'
+    });
+    $('#RightAccordion').accordion('add', {
+        title: '帮助',
+        content: "<p>Accordion is a part of easyui framework for jQuery. It lets you define your accordion component on web page more easily.</p>",
+        selected: false,
+        iconCls: 'icon-help'
+    });
+    $('#RightAccordion').accordion('add', {
+        title: '系统菜单',
+        content: "<p>Accordion is a part of easyui framework for jQuery. It lets you define your accordion component on web page more easily.</p>",
+        selected: false,
+        iconCls: 'icon-search'
+    });
+    $.parser.parse();
+});
