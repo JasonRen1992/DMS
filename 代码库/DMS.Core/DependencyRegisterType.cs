1e9962434eceb423dde9810b3f503fc6d34dc4ba
jason <190187776@qq.com>
2014-10-04 15:35:04 +0800

    1

---
+++ 代码库/DMS.Core/DependencyRegisterType.cs
@@ -0,0 +1,23 @@
+﻿using DMS.BLL;
+using DMS.DAL;
+using DMS.IBLL;
+using DMS.IDAL;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMS.Core
+{
+    public class DependencyRegisterType
+    {
+        //系统注入
+        public static void Container_Sys(ref UnityContainer container)
+        {
+            container.RegisterType<IHomeBLL, HomeBLL>();
+            container.RegisterType<IHomeRepository, HomeRepository>();
+        }
+    }
+}
