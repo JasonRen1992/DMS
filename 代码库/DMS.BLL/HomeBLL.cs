5d36a891b9815b1b85f3bec70ca5c1d4fd8a8b3c
jason <190187776@qq.com>
2014-10-04 15:34:28 +0800

    1

---
+++ 代码库/DMS.BLL/HomeBLL.cs
@@ -0,0 +1,17 @@
+﻿using DMS.IBLL;
+using DMS.IDAL;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMS.BLL
+{
+    public class HomeBLL : IHomeBLL
+    {
+        [Dependency]
+        public IHomeRepository HomeRepository { get; set; }
+    }
+}
