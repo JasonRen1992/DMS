da644cd64729978ac6c899bc63556deda40a539a
jason <190187776@qq.com>
2014-10-27 19:39:03 +0800

    aa

---
+++ 代码库/DMS.Common/Models/EmployeeEntity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using DMS.Common.Utils;
+
+namespace DMS.Common.Models
+{
+    public class EmployeeEntity
+    {
+        
+        [Display(Name = "Emp_Id")]
+        public string Emp_Id { get; set; }
+
+        [Display(Name = "Emp_Code")]
+        public string Emp_Code { get; set; }
+
+        [Display(Name = "Emp_Name")]
+        public string Emp_Name { get; set; }
+
+        [Display(Name = "Emp_Account")]
+        public string Emp_Account { get; set; }
+
+        [Display(Name = "Emp_PWD")]
+        public string Emp_PWD { get; set; }
+
+        [Display(Name = "Emp_LangCode")]
+        public string Emp_LangCode { get; set; }
+
+        [MaxWordsExpression(200)]
+        [Display(Name = "Emp_Email")]
+        public string Emp_Email { get; set; }
+
+        [MaxWordsExpression(100)]
+        [Display(Name = "Emp_Telephone")]
+        public string Emp_Telephone { get; set; }
+
+        [MaxWordsExpression(64)]
+        [Display(Name = "Emp_Sex")]
+        public string Emp_Sex { get; set; }
+
+        [MaxWordsExpression(64)]
+        [Display(Name = "Emp_Status")]
+        public string Emp_Status { get; set; }
+
+        [Display(Name = "IsEnable")]
+        public bool IsEnable { get; set; }
+
+    }
+}
